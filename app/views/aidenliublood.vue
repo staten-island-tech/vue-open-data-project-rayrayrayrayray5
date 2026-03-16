29e904e5c3f13afa6ad92b64da29cf30f168f89e
rayrayrayrayray5 <raymondl187@nycstudents.net>
2026-03-16 14:00:52 -0400

    aiden liu said he ate ethan xie's lambchops because he was on the grill but the lambchops were raw so aiden liu said he was on the toilet and it was like a picasso painting

---
+++ app/views/aidenliublood.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { onMounted, ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const scores = ref({})
+async function getScores(liu) {
+  const response = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
+  const data = await response.json()
+  scores.value = data
+}
+onMounted(function () {
+  getScores(route.params.liu)
+})
+watch(
+  () => route.params.liu,
+  function (liu) {
+    getScores(liu)
+  },
+)
+</script>
+
+<style scoped></style>
