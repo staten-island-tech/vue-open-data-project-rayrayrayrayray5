27637f03652144b8cd114019dbef1b196093ec61
rayrayrayrayray5 <raymondl187@nycstudents.net>
2026-03-18 13:37:17 -0400

    aidenliu

--- app/src/components/aidenliurawlambchops.vue
+++ app/src/components/aidenliurawlambchops.vue
@@ -1,16 +1,32 @@
 <template>
   <router-link :to="satPath" class="card">
-    <h2>{{ scores.value }}</h2>
+    <h2>{{ scores }}</h2>
   </router-link>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 const props = defineProps({
   scores: {
     type: Object,
     required: true,
   },
 })
+const satPath = computed(() => {
+  return `/${props.scores.name}`
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+.card {
+  width: 28%;
+  height: 500px;
+  background-color: aliceblue;
+  margin: 30px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  text-transform: uppercase;
+}
+</style>
--- app/src/views/aidenliublood.vue
+++ app/src/views/aidenliublood.vue
@@ -1,21 +1,19 @@
 <template>
   <div>
-    <h2>{{ scores.value }}</h2>
+    <h2>{{ scores }}</h2>
   </div>
 </template>
 
 <script>
 import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
-const route = useRoute()
 const scores = ref({})
 async function getScores() {
   const response = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
   const data = await response.json()
   scores.value = data
 }
-onMounted(function () {
-  getScores(route.params)
+onMounted(() => {
+  getScores()
 })
 </script>
 
