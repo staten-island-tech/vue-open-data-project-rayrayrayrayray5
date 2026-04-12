5ceed5cac94a234a2627054af5316d5f7f543503
rayrayrayrayray5 <raymondl187@nycstudents.net>
2026-04-12 00:41:37 -0400

    fsfs

--- app/src/views/aidenliublood.vue
+++ app/src/views/aidenliublood.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <SatChart :scores="scores" />
+    <input
+      :value="search"
+      @input="searchAidenLiu"
+      placeholder="Search For Schools"
+      class="aidenliusearchbox"
+    />
+    <SatChart :scores="filter" />
 
     <AidenLiu v-for="score in scores" :key="score.dbn" :scores="score" />
   </div>
@@ -12,14 +18,26 @@ import AidenLiu from '@/components/aidenliurawlambchops.vue'
 import SatChart from '@/components/ganbareganbare.vue'
 
 const scores = ref([])
+const search = ref('')
+function searchAidenLiu(gambare) {
+  search.value = gambare.target.value
+}
+
+onMounted(getScores)
+
+const filter = computed(() => {
+  const vincent = search.value.toLowerCase().trim()
+
+  if (!vincent) return scores.value
+
+  return scores.value.filter((whalen) => whalen.school_name?.toLowerCase().includes(vincent))
+})
 
 async function getScores() {
   const response = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
   const data = await response.json()
   scores.value = data
 }
-
-onMounted(getScores)
 </script>
 
 <style scoped></style>
